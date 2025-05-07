5c954e2cd6096b458a23fc01fe915270cc0f3719
Frank Erzoah-Cudjoe <frankcudjoe7@gmail.com>
2025-05-07 01:29:43 +0000

    initial-commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function revealSurprise() {
+//     document.getElementById("surprise-content").classList.remove("hidden");
+// }
+
+function showSurprise() {
+    document.getElementById("surprise-content").style.display = "block";
+}
+  
